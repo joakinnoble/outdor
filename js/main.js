d0b7f789887085cf6384c29143e9329270224c45
Bio_Tech <81431434+joakinnoble@users.noreply.github.com>
2023-01-09 00:54:03 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
-
+var divisor = document.getElementById("divisor"),
+slider = document.getElementById("slider");
+function moveDivisor() { 
+	divisor.style.width = slider.value+"%";
+}
 function main() {
 
 (function () {
